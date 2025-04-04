9cec5858a9a15451c38764415a1f9debc5f60473
Jason Campbell <jasoncampbell@Mac.lan>
2025-04-04 02:36:31 -0400

    getting through a few of these, KAHHHHHN

--- initialAssessment.py
+++ initialAssessment.py
@@ -4,7 +4,42 @@
 
 def check_solution(x):
     if x + 3 == 7:
-        print('true')
+        # print('true')
         return True
 
 check_solution(4)
+
+
+# second Question
+# 2. solve the equation 2x - 5 = 9
+# 
+# step 1 - add the 5 to both sides so 2x = 14
+# step 2 - divide each side by 2 so x = 7
+
+def check_second_solution(x):
+    if 2 * x - 5 == 9:
+        # print('true')
+        return True
+    else:
+        # print('false')
+        return False
+
+check_second_solution(7)
+
+
+# third question
+# solve for x
+# x^2 == 49
+# 
+# step 1 - square each side so x =7
+
+def check_third_solution(x):
+    if x**2 == 49:
+        print('true')
+        return True
+    else:
+        print('false')
+        return False
+
+check_third_solution(7)
+check_third_solution(-7)
