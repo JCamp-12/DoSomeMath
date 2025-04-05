d3485726007e9024875287d82467b65decb3142f
Jason Campbell <jasoncampbell@Mac.lan>
2025-04-04 23:05:54 -0400

    will use this as further testing

---
+++ initialAssessP2.py
@@ -0,0 +1,18 @@
+# fif question
+# 1. Solve for x
+#  4(x − 2) + 3 = 2x + 7
+# 
+# step 1 - simplify the left side of the equation so 4x - 8 +3 or 4x -5
+# step 2 - we can add 5 to each side so 4x = 2x + 12
+# step 3 - isolate x so we get x = 6
+
+def check_solution(x):
+    if 4 * (x - 2) + 3 == 2 * x + 7:
+        print('true')
+        return True
+
+check_solution(6)
+
+# simplify using foil
+# 3x^2 + 11x + 6
+#  
--- initialAssessment.py
+++ initialAssessment.py
@@ -35,11 +35,38 @@ check_second_solution(7)
 
 def check_third_solution(x):
     if x**2 == 49:
+        # print('true')
+        return True
+    else:
+        # print('false')
+        return False
+
+check_third_solution(7)
+check_third_solution(-7)
+
+
+# fourth problem
+# solve for x
+# 3x+2=x+8
+# 
+# step 1 - subtract 2 from each side so 3x = x +6
+# step 2 - subtract x from each side so 2x = 6
+# step 3 - divide each side by 2 so x = 3
+
+def check_fourth_solution(x):
+    if 3 * x + 2 == x + 8:
         print('true')
         return True
     else:
         print('false')
         return False
 
-check_third_solution(7)
-check_third_solution(-7)
+check_fourth_solution(3)
+check_fourth_solution(-3)
+
+
+# Word problem - too EZ... just looking for max bugs left.
+# She finds that each bug fix takes 15 minutes, and she’s already spent 45 minutes.
+# If she wants to spend no more than 2 hours total fixing bugs today... whats the max bugs she can still fix
+
+
